d4fcfa84c434fb593552ae0f1a6f04ac969b208c
Nikhil Haas <nikhil@nikhilhaas.com>
2023-01-31 11:24:52 -0800

    Add SARS-Cov-2 GPT2-generated Abs

--- src/biolm_util.py
+++ src/biolm_util.py
@@ -1,4 +1,73 @@
 import json, os, requests
+import urllib3
+import datetime
+import time
+
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+import logging
+
+log = logging.getLogger('biolm_util')
+
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=list(range(400, 599)),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+
+def retry_minutes(sess, URL, HEADERS, dat, timeout, mins):
+    """Retry for N minutes."""
+    try:
+        now = datetime.datetime.now()
+        try_until = now + datetime.timedelta(minutes=mins)
+        while datetime.datetime.now() < try_until:
+            response = None
+            try:
+                log.info('Trying {}'.format(datetime.datetime.now()))
+                response = sess.post(
+                    URL,
+                    headers=HEADERS,
+                    data=dat,
+                    timeout=timeout
+                )
+                response.raise_for_status()
+                if 'error' in response.json():
+                    raise ValueError(response.json().dumps())
+                else:
+                    break
+            except Exception as e:
+                log.warning(e)
+                if response:
+                    log.warning(response.text)
+                time.sleep(5)  # Wait 5 seconds between tries
+        if response is None:
+            err = "Got Nonetype response"
+            raise ValueError(err)
+        elif 'Server Error' in response.text:
+            err = "Got Server Error"
+            raise ValueError(err)
+        else:
+            response.raise_for_status()
+    except Exception as e:
+        raise
+    else:
+        return response
 
 
 def get_api_token():
@@ -54,8 +123,10 @@ def esm2_transform(seq):
       'Cookie': 'access={};refresh={}'.format(access, refresh),
       'Content-Type': 'application/json'
     }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
+    
+    session = requests_retry_session()
+    tout = urllib3.util.Timeout(total=720, read=360)
+    response = session.post(url, headers=headers, data=payload, timeout=tout)
 
     resp_json = response.json()
     transformations = resp_json['predictions']  # List, containing dicts for each sequence POSTed
@@ -92,7 +163,9 @@ def esmfold_pdb(seq):
       'Content-Type': 'application/json'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    session = requests_retry_session()
+    tout = urllib3.util.Timeout(total=180, read=180)
+    response = retry_minutes(session, url, headers, payload, tout, mins=12)
 
     resp_json = response.json()  
     # resp_json['predictions']  # List, one item for each sequence POSTed
