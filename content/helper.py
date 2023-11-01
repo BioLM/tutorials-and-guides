380014bf457f1cf06e96facdaa77d0268d51da77
Nikhil Haas <nikhil@nikhilhaas.com>
2023-11-01 07:43:23 -0700

    Add helper func imports and call in notebook to compile into pyodide

---
+++ content/helper.py
@@ -0,0 +1,18 @@
+async def jlite_post(model_slug, action, data, api_token):
+    action = action.strip().lower()
+    model_slug = model_slug.strip()
+    api_token = api_token.strip()
+    assert isinstance(data, dict)
+    data_str = json.dumps(data)
+
+    resp = await js.fetch(f'https://biolm.ai/api/v1/models/{model_slug}/{action}/', to_js({
+        "method": "POST",
+        "body": data_str,
+        "credentials": "same-origin",
+        "headers": {"Content-Type": "application/json",
+                    "Authorization": f"Token {api_token}"}
+    }, dict_converter=Object.fromEntries))
+
+    res = await resp.text()
+    json_resp = JSON(json.loads(res))
+    return json_resp
